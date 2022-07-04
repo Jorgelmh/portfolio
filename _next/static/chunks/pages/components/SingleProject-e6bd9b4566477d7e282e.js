24c8f03c458fede0d8502a5f3a8cf16f537cd1e2
Jorgelmh <42736565+Jorgelmh@users.noreply.github.com>
2022-07-04 22:45:33 +0000

    Deploying to gh-pages from  @ 4b7e1e95b1d547fce3a548c3d4e45f83d25cc961 🚀

---
+++ _next/static/chunks/pages/components/SingleProject-e6bd9b4566477d7e282e.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[15],{W83T:function(n,_,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components/SingleProject",function(){return o("x3t8")}])}},[["W83T",0,1,3]]]);
